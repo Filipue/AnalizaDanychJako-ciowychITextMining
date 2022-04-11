fdb366e4b138e9c60a3551dcc6b19619dd7aefa6
Filipue <57937089+Filipue@users.noreply.github.com>
2022-04-11 18:18:45 +0200

    Delete zad_3.py

--- zad_3.py
+++
@@ -1,8 +0,0 @@
-import re
-
-emotikony = "Lorem ypsum :) eros congue et. In blandit, mi eu porta;)" \
-            " ;( :> :< at tristique #frasistas augue risus" \
-            " eu risus ;< :-) ;-)"
-
-wyciaganie_emotikon = re.findall(r"[;|:][-]?\)|\(|<|>", emotikony)
-print(wyciaganie_emotikon)
